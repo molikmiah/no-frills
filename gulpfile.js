d2b6d339b514c5d00d2f2dc02d0bc63ec2d75e57
Molik Miah <molik.miah@replgroup.com>
2017-03-03 03:21:49 +0000

    created basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// require gulp packages
+var gulp  = require('gulp'),
+    gutil = require('gulp-util');
+
+// create a default task
+gulp.task('default', function() {
+  console.log('-------------------------------------------------');
+  console.log(' ');
+  console.log('no-frills.js');
+  console.log(' ');
+  console.log(' ');
+  console.log('available commands:');
+  console.log('gulp build');
+  gutconsoleil.log(' ');
+  console.log('-------------------------------------------------');
+});
+
+// build task
+gulp.task('build', function() {
+  console.log('-------------------------------------------------');
+  console.log(' ');
+  console.log('no-frills.js');
+  console.log(' ');
+  console.log(' ');
+  console.log('building framework for distribution...');
+  console.log(' ');
+  console.log('-------------------------------------------------');
+
+  // concat all required files in order
+
+  // minify
+
+  // output to build directory
+});
